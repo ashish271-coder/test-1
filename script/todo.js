a7ba7d7bc9b2c197f37d5c4647480649a6262812
Ashish Dawan <ashishdawan95@gmail.com>
2026-02-19 22:25:35 +0530

    First commit

---
+++ script/todo.js
@@ -0,0 +1,9 @@
+const array =[];
+function addList(){
+  const element = document.querySelector('.js-name');
+  const name = element.value;
+   
+  array.push(name);
+  console.log(array);
+  element.value ='';
+}
