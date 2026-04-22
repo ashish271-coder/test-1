ff03f7a6e1a3febcb14c807d6796cf2f5abba5cf
Ashish Dawan <ashishdawan95@gmail.com>
2026-04-23 00:24:21 +0530

    add dalic-date

--- data/cart.js
+++ data/cart.js
@@ -4,7 +4,7 @@
   cart = [   {
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
-  deliveryOptionId: '1'
+  deliveryOptionId: '3'
 },
  {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
@@ -45,3 +45,14 @@ if(cartIteam.productId !== productId){
 cart =newCart;
 saveToStorage();
 } 
+export function updatedeliveryOption(productId,deliveryOptionId){
+   let matchingIteam
+    cart.forEach((cartitem)=>{
+      if(productId===cartitem.productId){
+        matchingIteam =cartitem
+      }
+    });
+
+    matchingIteam.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
+}
--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,updatedeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import { removeFromCart } from '../data/cart.js';
@@ -89,7 +89,8 @@ deliveryOptions.forEach((deliveryOption) => {
 const ischecked = deliveryOption.id === cartItem.deliveryOptionId ;
 
   html += `
- <div class="delivery-option">
+ <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}"
+  data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio"
                   ${ischecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -120,3 +121,10 @@ document.querySelectorAll('.js-delete-link')
    container.remove();
   });
 }); 
+document.querySelectorAll('.js-delivery-option')
+.forEach((element)=>{
+element.addEventListener('click',() =>{
+  const {productId,deliveryOptionId} = element.dataset;
+updatedeliveryOption(productId,deliveryOptionId);
+});
+});
